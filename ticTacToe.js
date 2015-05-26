8805c6b1d58e5bb4c7f2ff0fa2cbd08b7cd3b264
Brandon <brandonlee503@gmail.com>
2015-05-26 15:55:37 -0700

    Initial commit

---
+++ ticTacToe.js
@@ -0,0 +1,18 @@
+function main(){
+    intro();
+    getNames();
+}
+
+
+function intro(){
+    console.log("Welcome to TicTacToe!");
+}
+
+function getNames(){
+    p1name = prompt("Player 1, please enter your name: ");
+    console.log("\n");
+    p2name = prompt("Player 2, please enter your name: ");
+    console.log("\n");
+}
+
+main();
