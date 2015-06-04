21372c1bb2e8e1f61edb2b2646ce68135449cad7
Brandon <brandonlee503@gmail.com>
2015-06-04 13:39:15 -0700

    'Started' #4

---
+++ 4_exercise.js
@@ -0,0 +1,27 @@
+//Import library
+var fs = require("fs");
+
+//Set the command line arguement
+theFilePath = process.argv[2];
+
+//Open and read the file
+var readFile = fs.readFileSync(theFilePath);
+
+//Convert to string from buffer
+var stringFile = readFile.toString();
+
+//Split string into array of substrings
+stringFile.split();
+
+console.log(stringFile);
+
+var theSum = 0;
+
+for (i in stringFile) {
+    if (stringFile[i] === "\n"){
+        theSum += 1;
+    }
+}
+
+//Print out total number of newlines from a file
+console.log(theSum);
