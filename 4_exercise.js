c51466d4e92d6ab4294cb3e662b658890cb68f19
Brandon <brandonlee503@gmail.com>
2015-06-10 15:46:20 -0700

    Finished #4

--- 4_exercise.js
+++ 4_exercise.js
@@ -1,27 +1,31 @@
 //Import library
 var fs = require("fs");
 
-//Set the command line arguement
-theFilePath = process.argv[2];
-
-//Open and read the file
-var readFile = fs.readFileSync(theFilePath);
-
-//Convert to string from buffer
-var stringFile = readFile.toString();
-
-//Split string into array of substrings
-stringFile.split();
-
-console.log(stringFile);
-
-var theSum = 0;
-
-for (i in stringFile) {
-    if (stringFile[i] === "\n"){
-        theSum += 1;
-    }
+//Read the file asynchronously
+function getFile(callback){
+    //Set the command line arguement
+    theFilePath = process.argv[2];
+    //Read the file async
+    fs.readFile(theFilePath, function doneReading(err, fileContents){
+        //Convert to string from buffer
+        var stringFile = fileContents.toString();
+        //Split the string and count newLines
+        stringFile.split();
+        var theSum = 0;
+        for (i in stringFile){
+            if (stringFile[i] === "\n"){
+                theSum += 1;
+            }
+        }
+        //Invoke callback (printNewLines) when finished with async task
+        callback(theSum);
+    })
 }
 
-//Print out total number of newlines from a file
-console.log(theSum);
+//The callback
+function printNewLines(theSum){
+    console.log(theSum);
+}
+
+//Call the function
+getFile(printNewLines);
