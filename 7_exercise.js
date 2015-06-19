bdc4ec1ff8eccbc0f694e234007c063e7de46441
Brandon <leebran@onid.oregonstate.edu>
2015-06-19 02:43:41 -0700

    Worked on #7 a bit more, got it to return response codes

--- 7_exercise.js
+++ 7_exercise.js
@@ -3,5 +3,7 @@ var http = require("http");
 var url = process.argv[2];
 
 http.get(url, function callback(response){
-    
+    console.log("Got response: " + response.statusCode);
+}).on("data", function (data){
+    console.log("Got data: " + data.message);
 });
