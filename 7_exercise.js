57964f22fbc2b9b9fcaa33ccb996632b40fba67f
Brandon <leebran@onid.oregonstate.edu>
2015-06-19 22:19:29 -0700

    Finished #7

--- 7_exercise.js
+++ 7_exercise.js
@@ -2,8 +2,11 @@ var http = require("http");
 
 var url = process.argv[2];
 
-http.get(url, function callback(response){
-    console.log("Got response: " + response.statusCode);
-}).on("data", function (data){
-    console.log("Got data: " + data.message);
+http.get(url, function callback0(response){
+
+    //Set encoding to have the node stream (response) return events as strings instead of buffer objects
+    response.setEncoding("utf8");
+    response.on("data", function callback1(data){
+        console.log(data);
+    });
 });
