e3b280fd871964aeb6f216e1364d74eeebd819da
Brandon <leebran@onid.oregonstate.edu>
2015-06-19 22:56:19 -0700

    Added more comments

--- 8_exercise.js
+++ 8_exercise.js
@@ -7,9 +7,13 @@ http.get(url, function callback0(response){
 
     //Set encoding to have the node stream (response) return events as strings instead of buffer objects
     response.setEncoding("utf8");
+
+    //Listen to and collect all data from stream and append to result as string
     response.on("data", function callback1(data){
         result += data;
     });
+
+    //Listen for end event to determine the end of data events, print data length and strings
     response.on("end", function callback2(){
         console.log(result.length);
         console.log(result);
